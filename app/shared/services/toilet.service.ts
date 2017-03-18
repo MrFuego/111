da91c4520c9f061f3f809a141fc64c2c38dd6aa8
Pieter Van Molle <van.molle.pieter@gmail.com>
2017-03-18 13:52:04 +0100

    Retrieve closest toilet

--- app/shared/services/toilet.service.ts
+++ app/shared/services/toilet.service.ts
@@ -9,19 +9,35 @@ export class ToiletService {
 
 	constructor(private http: Http) {
 	}
-
-	getToilets(): Observable<Toilet []> {
+	
+	/**
+	 * Returns an array of the format `[lat, lng, distance]` containing the
+	 * coordinates of the closest toilet to a given (lat, lng) pair.
+	 */
+	getToilet(lat: number, lng: number): Observable<Object[]> {
 		return this.http.get(this.toiletsUrl)
-						.map(res => res.json().coordinates);
-						//.map(toilets => toilets.map(this.toToilet))
-						//.catch(this.handleError);
+		.map(res => res.json().coordinates)
+		.map(toilets => toilets.map(t => [t[0], t[1],
+									      this.getDistanceFromLatLonInKm(t[0], t[1], lat, lng)]))
+		.map(toilets => toilets.sort((t1, t2) => t1[2] - t2[2]))
+		.map(toilets => toilets[0])
+		.catch(this.handleError);
 	}
 
-	private toToilet(toilet): Toilet {
-		return {
-			lat: toilet[0],
-			lng: toilet[1]
-		}
+	private getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+		var R = 6371; // Radius of the earth in km
+		var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+		var dLon = this.deg2rad(lon2-lon1);
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+				Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+				Math.sin(dLon/2) * Math.sin(dLon/2); 
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		var d = R * c; // Distance in km
+		return d;
+	}
+
+	private deg2rad(deg) {
+		return deg * (Math.PI/180)
 	}
 
 	private handleError(error: Response | any): Promise<any> {
