50f1190d834ee6797ce70a2c76652603ba497e6c
Matthias Seghers <matthiasseghers@hotmail.com>
2017-03-18 12:03:05 +0100

    update app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-<<<<<<< HEAD
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/observable/throw';
-=======
-import { CalculationService } from './shared/services/calculation.service';
 
->>>>>>> b2318844812aeba4179e7a278bc51a50b4d5e4a7
+import { CalculationService } from './shared/services/calculation.service';
 
 @NgModule({
   imports: [ 
