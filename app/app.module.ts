80c56bd1e915e5ba66b60caa7bb77f1f2e450bf2
Matthias Seghers <matthiasseghers@hotmail.com>
2017-03-18 11:59:42 +0100

    import rxjs operators

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/throw';
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule {}
