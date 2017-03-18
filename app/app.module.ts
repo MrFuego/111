b2318844812aeba4179e7a278bc51a50b4d5e4a7
Jef Roosens <jef.roosens@gmail.com>
2017-03-18 11:41:20 +0100

    [feature] add CalculationService to module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { CalculationService } from './shared/services/calculation.service';
+
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [ 
+    BrowserModule
+   ],
+  declarations: [ 
+    AppComponent
+    ],
+  providers: [
+    CalculationService
+  ],
+  bootstrap: [ 
+    AppComponent 
+    ]
 })
 export class AppModule {}
