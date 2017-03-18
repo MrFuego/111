95d628ad7d1623bc24702d9a8d17d7578b407fe6
Matthias Seghers <matthiasseghers@hotmail.com>
2017-03-18 15:10:41 +0100

    add service beer

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -7,39 +7,66 @@ import { ToiletService } from '../shared/services/toilet.service';
 import { Place } from '../shared/models/place';
 
 @Component({
-    templateUrl: './app/home/home.component.html',
+  templateUrl: './app/home/home.component.html',
 })
-export class HomeComponent{
-  constructor( private bs: BeerService,
-              private cs: CalculationService,
-              private gs: GeolocationService,
-              private ts: ToiletService )
-  {
+export class HomeComponent {
+  constructor(private bs: BeerService,
+    private cs: CalculationService,
+    private gs: GeolocationService,
+    private ts: ToiletService) {
   }
-  
+
 
   getToilet() {
-  	let userLocation = new Place;
-	  let toiletLocation = new Place;
-  
-  	this.gs.getLocation().subscribe(
+    let userLocation = new Place;
+    let toiletLocation = new Place;
+
+    this.gs.getLocation().subscribe(
       data => {
         userLocation.lat = data.coords.latitude;
-	    userLocation.long = data.coords.longitude;
+        userLocation.long = data.coords.longitude;
 
-	    this.ts.getToilet(userLocation.long, userLocation.lat).subscribe(
-		  res => {
-		    toiletLocation.lat = <number> res[0];
-			toiletLocation.long = <number> res[1];
-			let url = this.createUrl(userLocation.lat, userLocation.long, toiletLocation.long, toiletLocation.lat);
+        this.ts.getToilet(userLocation.long, userLocation.lat).subscribe(
+          res => {
+            toiletLocation.lat = <number>res[0];
+            toiletLocation.long = <number>res[1];
+            let url = this.createUrl(userLocation.lat, userLocation.long, toiletLocation.long, toiletLocation.lat);
             window.open(url);
-		  }
-		)
+          }
+        )
       }
     )
   }
 
-  createUrl(latFrom:number, lonFrom:number, latTo:number, lonTo:number) {
+  getCafe() {
+    let userLocation = new Place;
+    let cafeLocation = new Place;
+    let userWeight = (<HTMLInputElement>document.getElementById('zwaargewicht')).value;
+
+    this.gs.getLocation().subscribe(
+      data => {
+        userLocation.lat = data.coords.latitude;
+        userLocation.long = data.coords.longitude;
+
+        var dist = this.cs.getDistanceToRun(+userWeight);
+
+
+        console.log(dist);
+        console.log(this.bs.getCafe(userLocation.long, userLocation.lat, dist));
+        this.bs.getCafe(userLocation.long, userLocation.lat, dist).subscribe(
+          res => {
+            console.log("pint");
+            // cafeLocation.lat = <number>res[0];
+            // cafeLocation.long = <number>res[1];
+            // let url = this.createUrl(userLocation.lat, userLocation.long, cafeLocation.long, cafeLocation.lat);
+            // window.open(url);
+          }
+        )
+      }
+    )
+  }
+
+  createUrl(latFrom: number, lonFrom: number, latTo: number, lonTo: number) {
     return `https://www.google.com/maps/dir/${latFrom},${lonFrom}/${latTo},${lonTo}/data=!4m2!4m1!3e2`;
   }
 }
